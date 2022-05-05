2af1de7444c330508287ee7893ac4fee3f5cd91b
David Garcia <a20davgaralo@insperalbes.cat>
2022-05-05 10:39:13 +0200

    Subido pom.xml

--- src/main/java/com/example/ftp/controllers/LoginController.java
+++ src/main/java/com/example/ftp/controllers/LoginController.java
@@ -1,4 +1,19 @@
 package com.example.ftp.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class LoginController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String login(Model model) {
+
+
+        model.addAttribute("titulo", "Login del FTP");
+        return "login";
+    }
 }
